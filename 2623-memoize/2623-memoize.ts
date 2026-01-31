685fd52a07605f20463b93ba20c0d8932e0ca328
Nadeem Ahmad <Nddar191131@gmail.com>
2026-01-31 22:08:52 +0530

    Time: 253 ms (72.45%), Space: 95.7 MB (61.75%) - LeetHub

---
+++ 2623-memoize/2623-memoize.ts
@@ -0,0 +1,30 @@
+type Fn = (...params: number[]) => number
+
+function memoize(fn: Fn): Fn {
+    const cache = new Map();
+  let callCount = 0;
+    return function(...args) {
+        const key = JSON.stringify(args);
+
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+
+    callCount++;
+    const result = fn(...args);
+    cache.set(key, result);
+    return result;
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
